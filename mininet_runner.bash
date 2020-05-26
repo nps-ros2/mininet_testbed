0996ce31d8629d459c702f82eb5591ea6c63301e
Bruce Allen <bdallen@nps.edu>
2020-05-26 11:53:41 -0700

    fix parameter usage

--- mininet_runner.bash
+++ mininet_runner.bash
@@ -2,10 +2,23 @@
 # usage: minininet_runner.py <setup file> <output file> [-t "<option ...>"]
 echo Command: ./mininet_runner.py $1 $2 $3 "$4"
 
-sudo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" \
-     "AMENT_PREFIX_PATH=${AMENT_PREFIX_PATH}" \
-     "COLCON_PREFIX_PATH=${COLCON_PREFIX_PATH}" \
-     "PYTHONPATH=${PYTHONPATH}" \
-     "PATH=${PATH}" \
-     ./mininet_runner.py $1 $2 $3 "$4"
+if [[ -z $3 && -z $4 ]]; then
+
+    sudo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" \
+         "AMENT_PREFIX_PATH=${AMENT_PREFIX_PATH}" \
+         "COLCON_PREFIX_PATH=${COLCON_PREFIX_PATH}" \
+         "PYTHONPATH=${PYTHONPATH}" \
+         "PATH=${PATH}" \
+         ./mininet_runner.py $1 $2
+
+else
+
+    sudo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" \
+         "AMENT_PREFIX_PATH=${AMENT_PREFIX_PATH}" \
+         "COLCON_PREFIX_PATH=${COLCON_PREFIX_PATH}" \
+         "PYTHONPATH=${PYTHONPATH}" \
+         "PATH=${PATH}" \
+         ./mininet_runner.py $1 $2 $3 "$4"
+
+fi
 
