5bd1a204112d3eba472718fee5a8c6931bd28543
Bruce Allen <bdallen@nps.edu>
2020-07-28 11:05:59 -0700

    add scenario

---
+++ scenarios/search_and_rescue.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+from mininet.node import Host, Node
+from mininet.log import setLogLevel, info
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.node import Station, OVSKernelAP
+from mn_wifi.cli import CLI
+from mn_wifi.link import wmediumd
+from mn_wifi.wmediumdConnector import interference
+from subprocess import call
+
+
+def myNetwork():
+
+    net = Mininet_wifi(topo=None,
+                       build=False,
+                       link=wmediumd,
+                       wmediumd_mode=interference,
+                       ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    info( '*** Add switches/APs\n')
+    ap3 = net.addAccessPoint('ap3', cls=OVSKernelAP, ssid='ap3-ssid',
+                             channel='1', mode='g', position='599.0,132.0,0')
+    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, ssid='ap2-ssid',
+                             channel='1', mode='g', position='458.0,213.0,0')
+    r2 = net.addHost('r2', cls=Node, ip='0.0.0.0')
+    r2.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r4 = net.addHost('r4', cls=Node, ip='0.0.0.0')
+    r4.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r3 = net.addHost('r3', cls=Node, ip='0.0.0.0')
+    r3.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    info( '*** Add hosts/stations\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+
+    info("*** Configuring Propagation Model\n")
+    net.setPropagationModel(model="logDistance", exp=3)
+
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+
+    info( '*** Add links\n')
+    net.addLink(h1, ap3)
+    net.addLink(ap3, h3)
+    net.addLink(ap3, h4)
+    net.addLink(r1, r2)
+    net.addLink(r2, r3)
+    net.addLink(r1, h2)
+    net.addLink(r3, r4)
+    net.addLink(r4, ap2)
+    net.addLink(ap2, h1)
+
+    net.plotGraph(max_x=1000, max_y=1000)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches/APs\n')
+    net.get('ap3').start([])
+    net.get('ap2').start([])
+
+    info( '*** Post configure nodes\n')
+
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
